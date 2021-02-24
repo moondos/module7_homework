2c8af23b29c366a2c9a16a4d03e1137acc8d288e
Aidos Kenessov <47719645+moondos@users.noreply.github.com>
2021-02-24 19:33:16 +0000

    Create Task 4.js

---
+++ Task 4.js
@@ -0,0 +1,55 @@
+function ElectricalDevice() {
+    this.nomination = "Электроприбор";
+}
+
+ElectricalDevice.prototype.plugIn = function () {
+    console.log(`Прибор включен в розетку`);
+}
+
+ElectricalDevice.prototype.unPlug = function () {
+    console.log(`Прибор выключен из розетки`);
+}
+
+function Kettle(brand, color, price, voltage, power) {
+    this.brand = brand;
+    this.color = color;
+    this.price = price;
+    this.voltage = voltage;
+    this.power = power;
+}
+
+Kettle.prototype = new ElectricalDevice();
+
+Kettle.prototype.boilWater = function () {
+    console.log("Water is boiled")
+}
+
+Kettle.prototype.display = function () {
+    this.energyConsumption = this.voltage * this.power;
+    return(`${this.nomination} - чайник ${this.brand}: потребление энергии - ${this.energyConsumption}W, цвет - ${this.color}, цена - ${this.price}тг.`)
+}
+
+function MicroWave(brand, color, price, voltage, power) {
+    this.brand = brand;
+    this.color = color;
+    this.price = price;
+    this.voltage = voltage;
+    this.power = power;
+}
+
+MicroWave.prototype = new ElectricalDevice();
+
+MicroWave.prototype.readyFood = function () {
+    console.log("Food is ready")
+}
+
+MicroWave.prototype.display = function () {
+    this.energyConsumption = this.voltage * this.power;
+    return(`${this.nomination} - микроволновка ${this.brand}: потребление энергии - ${this.energyConsumption}W, цвет - ${this.color}, цена - ${this.price}тг.`)
+}
+
+const kettleBosh = new Kettle("Bosh", "Green", 5000, 100, 2);
+console.log(kettleBosh.display())
+
+const MicroWaveLG = new MicroWave("LG", "White", 3000, 200, 3);
+console.log(MicroWaveLG.display())
